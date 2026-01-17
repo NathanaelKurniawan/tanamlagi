482f17d6dddcf76bbdd086f3e5b8cab0bb92feea
NathanaelKurniawan <nathanaelkurniawan.nk@gmail.com>
2026-01-17 12:35:03 +0700

    Initial commit Laravel TanamLagi

---
+++ database/migrations/2026_1_3_093130_organization.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('location');
+            $table->string('focus_area');
+            $table->string('organization_type');
+            $table->string('logo_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        //
+    }
+};
